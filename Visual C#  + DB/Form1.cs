08b06e9d79176213cf2075fcbf78369ea3ddbed4
MiloDS <152004293+MiloDS@users.noreply.github.com>
2024-09-25 16:40:23 -0500

    Update Form1.cs
    Agregar comentarios

--- Visual C#  + DB/Form1.cs
+++ Visual C#  + DB/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+//Alexander Vargas Mejia
+//18-09-2024
+//visual C# + DB
 
 namespace Visual_C_____DB
 {
